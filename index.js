d29053f20254c4f0c6c2699773c2f375ec1fcd3e
Manuel Castillo <mac.jmanuel@gmail.com>
2020-10-07 18:14:03 -0500

    *added* pokedex.json

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
 const app = express()
+const { pokemon } = require('./pokedex.json')
 
 app.get('/', (req, res, next) => {
     res.status(200)
-    res.send('Hola Mundo')
+    res.send('Bienvenido')
 })
 
-app.listen('3000', () => {
+app.get('/pokemon/all', (req, res, next) => {
+    res.status(200)
+    res.send(pokemon)
+})
+
+app.get('/pokemon/:id([0-9]{1,3})', (req, res, next) => {
+    const { id } = req.params
+    if(id > 0 && id <= 151){
+        res.status(200)
+        res.send(pokemon[id - 1])
+    } 
+    else {
+        res.status(404)
+        res.send('Ese pokemon no existe')
+    }
+})
+
+app.get('/pokemon/:name', (req, res, next) => {
+    const { name } = req.params
+    res.status(200)
+    res.send(pokemon.find(poke => poke.name = name))
+})
+
+app.listen(process.env.PORT || '3000', () => {
     console.log('Servidor activo...')
 })
