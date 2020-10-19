c0ba0a611d0d60ac2e001f6dd45c219ecb3bb9b4
Manuel Castillo <mac.jmanuel@gmail.com>
2020-10-19 18:15:59 -0500

    'actividad8'

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const express = require('express')
-const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const app = express()
 const pokemon = require('./routes/pokemon')
 
 // para que anadir middlewares
 app.use(morgan('dev'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true}))
+app.use(express.json())
+app.use(express.urlencoded({ extended: true}))
 
 app.get('/', (req, res, next) => {
-    res.status(200).send('Bienvenido')
+    res.status(200).json({
+        code: 1,
+        message: 'Bienvenido' 
+    })
 })
 
 app.use('/pokemon', pokemon)
+
+app.use((req, res, next) => {
+    res.status(404).json({
+        code: 404,
+        message: 'Not Found'
+    })
+})
 
 app.listen(process.env.PORT || '3000', () => {
     console.log('Servidor activo...')
--- routes/pokemon.js
+++ routes/pokemon.js
@@ -4,7 +4,10 @@ const db = require('../config/database')
 
 pokemon.get('/', async (req, res, next) => {
     const pokedex = await db.query('SELECT * FROM pokemon')
-    res.status(200).json(pokedex)
+    res.status(200).json({
+        code: 1,
+        message: pokedex
+    })
 })
 
 pokemon.post('/', (req, res, next) => {
@@ -16,16 +19,28 @@ pokemon.get('/:id([0-9]{1,3})', async (req, res, next) => {
     const { id } = req.params
     const poke = await db.query(`SELECT * FROM pokemon WHERE pok_id = ${id}`)
     poke.length > 0 
-    ? res.status(200).send(poke)
-    : res.status(404).send('Ese pokemon no existe')
+    ? res.status(200).json({
+        code: 1,
+        message: poke
+    })
+    : res.status(404).json({
+        code: 404,
+        message: 'Ese pokemon no existe'
+    })
 })
 
 pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     const { name } = req.params
     const poke = await db.query(`SELECT * FROM pokemon WHERE pok_name LIKE '${name}'`)
     poke.length > 0
-    ? res.status(200).send(poke) 
-    : res.status(404).send('Ese pokemon no existe')
+    ? res.status(200).json({
+        code: 1,
+        message: poke
+    })
+    : res.status(404).json({
+        code: 404,
+        message: 'Ese pokemon no existe'
+    })
 })
 
 module.exports = pokemon
