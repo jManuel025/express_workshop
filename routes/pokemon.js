89302bfaa1438c9af9b931ea85667b43997db755
Manuel Castillo <mac.jmanuel@gmail.com>
2020-10-26 16:38:46 -0600

    'actividad10'

--- routes/pokemon.js
+++ routes/pokemon.js
@@ -60,4 +60,64 @@ pokemon.get('/:name([A-Za-z]+)', async (req, res, next) => {
     })
 })
 
+pokemon.delete('/:id([0-9]{1,3})', async (req, res, next) => {
+    const { id } = req.params
+    const query = `DELETE FROM pokemon WHERE pok_id = ${id}`
+    const rows = await db.query(query)
+    rows.affectedRows
+    ? res.status(200).json({
+        code: 200,
+        message: 'Pokemon borrado correctamente'
+    })
+    : res.status(404).json({
+        code: 404,
+        message: 'Pokemon no encontrado'
+    })
+})
+
+pokemon.put('/:id([0-9]{1,3})', async (req, res, next) => {
+    const { id } = req.params
+    const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body
+
+    if (pok_name && pok_height && pok_weight && pok_base_experience ) {
+        let query = `UPDATE pokemon SET pok_name='${pok_name}',pok_height=${pok_height},pok_weight=${pok_weight},pok_base_experience=${pok_base_experience} WHERE pok_id = ${id}`
+        const rows = await db.query(query)
+        rows.affectedRows == 1
+        ? res.status(200).json({
+            code: 200,
+            message: 'Pokemon actualizado correctamente'
+        })
+        : res.status(404).json({
+            code: 404,
+            message: 'Falló actualizacion'
+        })
+    }
+    res.status(500).json({
+        code: 500,
+        message: 'Campos incompletos'
+    })
+})
+
+pokemon.patch('/:id([0-9]{1,3})', async (req, res, next) => {
+    const { id } = req.params
+    const { pok_name } = req.body
+    if(pok_name){
+        let query = `UPDATE pokemon SET pok_name='${pok_name}' WHERE pok_id = ${id}`
+        const rows = await db.query(query)
+        rows.affectedRows == 1
+        ? res.status(200).json({
+            code: 200,
+            message: 'Pokemon actualizado correctamente'
+        })
+        : res.status(500).json({
+            code: 500,
+            message: 'Ocurrio un error'
+        })
+    }
+    res.status(500).json({
+        code: 500,
+        message: 'Campos incompletos'
+    })
+})
+
 module.exports = pokemon
